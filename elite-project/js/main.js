e4550e2139a245a8cec0ada33681e4696646b3f7
RPRebellatto <rogerio.rebellatto@gmail.com>
2021-01-06 14:48:49 -0300

    Trying to correct onload errors

--- elite-project/js/main.js
+++ elite-project/js/main.js
@@ -1,15 +1,19 @@
-var time = new Date();
+var d = new Date();
 
-const charList = [{name: "Heiel", story: "Heiel was a curious case."},
+var charList = [{name: "Heiel", story: "Heiel was a curious case."},
 {name: "Elros", story: "Elros was a more serious and good elf. Still, from a good heart and virtuos values he fell into de depths of despair, becaming the bringer of tyranny"},
 {name: "Night", story: "Night was a lost child, searching for the truth behind his existence, he came to a revelation of his undead body"},
 {name: "Ghanis", story: "Ghanis was a crazy fire mage, his mind almost fully filled with insanity and the will to burn everything in sight, still have a little space for friendship and evolution"}
 ];
 
-document.getElementById("page-time").innerHTML = time;
-//por que esses codigos não estão sendo reconhecidos/ dando erro?
-document.getElementById("charstory").innerHTML = charList[0].story;
+alert(d.getHours);
 
+function initPage(){
+    document.getElementById("page-time").innerHTML = d.getHours + ":" + d.getMinutes;
+    document.getElementById("charstory").innerHTML = charList[0].story;
+}
+
+window.onload = initPage;
 
 function getStory(){
 document.getElementById("charstory").innerHTML = charList[3].story;
