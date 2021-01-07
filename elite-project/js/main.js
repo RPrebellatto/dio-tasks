d1ef7e82826df2a4e4ca8977785ee1d9b501e75c
RPRebellatto <rogerio.rebellatto@gmail.com>
2021-01-07 20:07:36 -0300

    Corrections on Date() error

--- elite-project/js/main.js
+++ elite-project/js/main.js
@@ -1,21 +1,21 @@
 var d = new Date();
 
-var charList = [{name: "Heiel", story: "Heiel was a curious case."},
+var charList = [
+{name: "Character", story: "Character Story"},
+{name: "Heiel", story: "Heiel was a curious case."},
 {name: "Elros", story: "Elros was a more serious and good elf. Still, from a good heart and virtuos values he fell into de depths of despair, becaming the bringer of tyranny"},
 {name: "Night", story: "Night was a lost child, searching for the truth behind his existence, he came to a revelation of his undead body"},
 {name: "Ghanis", story: "Ghanis was a crazy fire mage, his mind almost fully filled with insanity and the will to burn everything in sight, still have a little space for friendship and evolution"}
 ];
 
-alert(d.getHours);
-
 function initPage(){
-    document.getElementById("page-time").innerHTML = d.getHours + ":" + d.getMinutes;
+    document.getElementById("page-time").innerHTML = d.getHours() + ":" + d.getMinutes();
     document.getElementById("charstory").innerHTML = charList[0].story;
 }
 
 window.onload = initPage;
 
-function getStory(){
-document.getElementById("charstory").innerHTML = charList[3].story;
+function getStory(n){
+document.getElementById("charstory").innerHTML = charList[n].story;
 }
 
